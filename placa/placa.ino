d784d5cc9e029b40f5a22e416f572eda965b7b06
Kevin <kevinsadao@hotmail.com>
2017-11-07 18:22:51 -0200

    Melhorias no codigo

--- placa/placa.ino
+++ placa/placa.ino
@@ -2,23 +2,25 @@
 #include <UIPEthernet.h>
 #include <utility/logging.h>
 #include <SPI.h>
+#ifdef ONLINE
 #include <PubSubClient.h>
+#else
+#include <SerialPubSubClient.h>
+#endif
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
 const int rs = 9, en = 8, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0x10 };
 const char* mqtt_server = "test.mosquitto.org";
 int vagaDisponivel;
 int vagaOcupada;
-boolean retainedMessage;
 
 const int pinLed = 7;
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String mensagem;
   String topicStr;
+
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -28,11 +30,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 
   Serial.println(mensagem);
-  
+
   Serial.println();
 
   for (int i = 0; i < length; i++) {
-    topicStr = (char)topic[i];
+    topicStr = topicStr + (char)topic[i];
   }
 
   Serial.println(topicStr);
@@ -60,27 +62,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 
   /*
-  if (topicStr == "senai-code-xp/vagas/10") {
+    if (topicStr == "senai-code-xp/vagas/10") {
     Serial.println("Esta Ã© mensagem que vei do mosquito:");
     Serial.println(mensagem);
-  }
-  else
-  {
+    }
+    else
+    {
     Serial.println("Nao recebi nenhuma mensagem neste topico");
     Serial.println(topicStr);
-  }
+    }
 
-  if (mensagem == 0) {
+    if (mensagem == 0) {
     Serial.println("Não disponível");
     vagaOcupada += 1;
 
-  } else if (mensagem == 1) {
+    } else if (mensagem == 1) {
     Serial.println("Disponível");
     vagaDisponivel += 1;
-  }
+    }
 
-  Serial.println("Vaga Ocupada:" + vagaOcupada);
-  Serial.println("Vaga Disponível:" + vagaDisponivel);
+    Serial.println("Vaga Ocupada:" + vagaOcupada);
+    Serial.println("Vaga Disponível:" + vagaDisponivel);
   */
 
   delay(5000);
@@ -93,7 +95,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(pinLed, OUTPUT);
 
-  //client.willRetain = true;
+  Serial.print("oi");
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -101,40 +103,36 @@ void setup() {
   //lcd.print("hello, world!");
   lcd.print("Kevin");
 
+  Serial.println();
+
   if (!Ethernet.begin(mac)) {
     Serial.println("Falha no DHCP");
   } else {
     Serial.println(Ethernet.localIP());
   }
 
-  retainedMessage = true;
+  //delay(1000);
 
-  delay(1000);
-
-  if (client.connect("Placa0X10")) {
-    Serial.println("Cheguei aqui");
-    // Serial.flush();
-    client.publish("senai-code-xp/vagas/10", "vaicorinthians", retainedMessage);
-    client.subscribe("senai-code-xp/vagas/#");
-    client.setCallback(callback);
-
-  }
-  else {
-    Serial.println("Nao conectei ao servidor MQTT");
-  }
 }
 
 void reconnect() {
   // Loop until we're reconnected
+  Serial.print("opa");
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
     // Attempt to connect
-    if (client.connect("Placa")) {
+    if (client.connect("Placa0X10")) {
+      /*if (client.connect("Placa0X10",
+                         "luz",
+                         1,
+                         true,
+                         "Morreu"
+                        )) {*/
       Serial.println("connected");
       delay(10 * 1000);
       // Once connected, publish an announcement...
-      client.publish("senai-code-xp/vagas/10", "vaicorinthians", retainedMessage);
+      client.publish("senai-code-xp/vagas/10", "vaicorinthians", true);
       client.subscribe("senai-code-xp/vagas/#");
 
     } else {
@@ -153,7 +151,6 @@ void loop() {
     reconnect();
   }
 
-  digitalWrite(pinLed, HIGH);
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
