22f53420e12c876efa2c635886a6d8e55aff9d03
Kevin <kevinsadao@hotmail.com>
2017-11-08 20:47:24 -0200

    lcd e led

--- placa/placa.ino
+++ placa/placa.ino
@@ -217,7 +217,13 @@ void loop() {
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  lcd.print("Vagas livres: ");
+  lcd.print(vagaDisponivel);
+
+  if(vagaDisponivel == 0){
+    digitalWrite(pinLed, HIGH);
+  }
+  
   client.loop();
 
 }
