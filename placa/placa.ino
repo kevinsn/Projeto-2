6ed16fe504c48186acab7152ec045ce1337542b8
Kevin <kevinsadao@hotmail.com>
2017-11-09 19:13:18 -0200

    Pequena alteracao +remocao newTopic

--- placa/placa.ino
+++ placa/placa.ino
@@ -18,17 +18,9 @@ int vagaOcupada;
 const int pinLed = 7;
 const int pinLcd = A0;
 int vagas[41];
-int newTopic;
-
-unsigned long timeAtualiza = 0; // will store last time LED was updated
-// constants won't change :
-const long interval = 1000; // interval at which to blink (milliseconds)
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  timeAtualiza = millis();
-  lcd.display();
-  digitalWrite(pinLcd, HIGH);
-  // ,,,,, acender visor...
+
   Serial.println("Estou no callback");
 
   char* mensagem = (char)payload[0] - 48;
@@ -87,9 +79,9 @@ void setup() {
   for (int i = 0; i < 41; i++) {
     vagas[i] = -1;
   }
-
+  
   pinMode(pinLcd, OUTPUT);
-
+    
   Serial.print("oi");
 
   // set up the LCD's number of columns and rows:
@@ -125,11 +117,10 @@ void setup() {
     Serial.println("try again in 5 seconds");
     // Wait 5 seconds before retrying
     delay(1000);
-
   }
 
   //delay(1000);
-  timeAtualiza = millis();
+
 }
 
 void reconnect() {
@@ -163,18 +154,13 @@ void reconnect() {
 }
 
 void loop() {
-  int now = millis();
-  if (now - timeAtualiza > interval) {
-    Serial.println("Apagando LCD"); // colocar aqui o codigo para apagar o LED.
-    lcd.noDisplay();
-    digitalWrite(pinLcd, LOW);
-    timeAtualiza = millis();
-  }
 
   if (!client.connected()) {
     reconnect();
   }
 
+  lcd.clear();
+  digitalWrite(pinLcd, LOW);
 
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
